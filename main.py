eaed37ed269f36723e113f8c705dfab15ea4bf88
Markos Narinian <manarinian@gmail.com>
2026-07-02 15:25:25 +0300

    Revert "Add a --dev CLI flag to enable dev mode"
    This reverts commit f03fb4df01adddb0711ccfaf71f33d80235fedfc.

--- main.py
+++ main.py
@@ -1,17 +1,8 @@
-import argparse
-
 from app import PokertoolsApp
 
 
-def is_dev_mode_active():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--dev", action="store_true")
-    args = parser.parse_args()
-    return args.dev
-
-
 def main():
-    app = PokertoolsApp(dev_mode=is_dev_mode_active())
+    app = PokertoolsApp()
     app.run()
 
 
